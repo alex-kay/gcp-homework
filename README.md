84307ab237cb1e7cb34d3cea44af1674b28d1a05
alex-kay <ichijoint@gmail.com>
2021-07-31 18:01:27 +0300

    added tomcat startup script

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # gcp-homework
+
+## steps
+
+1. Create bucket for application files and another one for static web files (think about permissions)
+2. create MIG for backend with installed tomcat and on boot download demo application from bucket
+3. setup autoscaling buy CPU (think about scale down)
+​4. create LB
+​5. add one more MIG for frontend with nginx, by path /demo/ show demo app from bucket, by path /img/picture.jpg show file from bucket
+​6. setup export of nginx logs to bucket/BigQuery
+​7. SSL terination (bonus)
+
+## 1. Create bucket for application files and another one for static web files (think about permissions)
+
+```bash
+gsutil mb gs://gcp-hw-app-bucket
+gsutil mb gs://gcp-hw-web-bucket
+curl -O https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/sample.war
+gsutil cp sample.war gs://gcp-hw-app-bucket
+```
+
+## 2. create MIG for backend with installed tomcat and on boot download demo application from bucket
+
+```bash
+
+```
---
+++ tomcat-startup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install default-jdk curl -y
+sudo groupadd tomcat
+sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
+cd /tmp
+curl -O 'https://apache.paket.ua/tomcat/tomcat-9/v9.0.50/bin/apache-tomcat-9.0.50.tar.gz'
+sudo mkdir /opt/tomcat
+sudo tar -xzvf apache-tomcat-9*tar.gz -C /opt/tomcat --strip-components=1
+cd /opt/tomcat
+sudo chgrp -R tomcat /opt/tomcat
+sudo chmod -R g+r conf
+sudo chmod g+x conf
+sudo chown -R tomcat webapps/ work/ temp/ logs/
+cat << EOF > /etc/systemd/system/tomcat.service
+[Unit]
+Description=Apache Tomcat Web Application Container
+After=network.target
+
+[Service]
+Type=forking
+
+Environment=JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64
+Environment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid
+Environment=CATALINA_HOME=/opt/tomcat
+Environment=CATALINA_BASE=/opt/tomcat
+Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
+Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
+
+ExecStart=/opt/tomcat/bin/startup.sh
+ExecStop=/opt/tomcat/bin/shutdown.sh
+
+User=tomcat
+Group=tomcat
+UMask=0007
+RestartSec=10
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+gsutil cp gs://gcp-hw-app-bucket/sample.war /opt/tomcat/webapps/
+chown tomcat:tomcat /opt/tomcat/webapps/sample.war
+
+sudo systemctl daemon-reload
+sudo systemctl start tomcat
+sudo systemctl enable tomcat
+
