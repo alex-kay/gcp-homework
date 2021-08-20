d47d343a5c2ac4996ce30ca4aa524894e5671fd2
Oleksandr Kurylo <oleksandr_kurylo@epam.com>
2021-08-20 04:06:42 +0300

    fixed elk instance script

--- elk-startup.sh
+++ elk-startup.sh
@@ -8,19 +8,27 @@ sudo apt update
 sudo apt install default-jre -y
 sudo apt install elasticsearch -y
 
-sudo cat << EOF > /etc/elasticsearch/elasticsearch.yml
+sudo cat << EOF >> /etc/elasticsearch/elasticsearch.yml
 network.host: 0.0.0.0
 http.port: 9200
 cluster.initial_master_nodes: ["$(hostname -I | awk '{gsub(/[ \t]+$/,""); print $0}')"]
 EOF
 
+sudo service elasticsearch start
+sudo systemctl enable elasticsearch
+
 sudo apt install kibana -y
+
+sudo cat << EOF >> /etc/kibana/kibana.yml
+server.host: "0.0.0.0"
+server.port: 5601
+elasticsearch.hosts: ["http://localhost:9200"]
+EOF
+
+sudo service kibana start
+sudo systemctl enable kibana
 
 sudo apt install logstash -y
 
-# sudo service elasticsearch start
-# sudo systemctl enable elasticsearch
-# sudo service kibana start
-# sudo systemctl enable kibana
-# sudo service logstash start
-# sudo systemctl enable logstash
+sudo service logstash start
+sudo systemctl enable logstash
